c715b7af4a357ef300ddd129e871b2a4791623a8
Luigi Fedele <luigiffedele@outlook.com>
2025-05-21 19:47:44 -0300

    config: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
 import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
   { 
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"],
+    plugins: { 
+      js,
+      prettier,
+     }, 
+    extends: [
+      "js/recommended",
+      "plugin:prettier/recommended",
+    ],
+    rules: {
+      "prettier/prettier": "error",
+    },
     ignores: ["node_modules/**", "dist/**", "build/**"],
   },
   { 
